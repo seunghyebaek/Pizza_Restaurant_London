6749a511a3642626e9f64ea819cbfa5e68aab37b
SH Baek <seunghyebaek0509@gmail.com>
2022-05-02 13:57:18 +0100

    first commit

--- pages/cart.jsx
+++ pages/cart.jsx
@@ -166,8 +166,7 @@ const Cart = () => {
               </button>
               <PayPalScriptProvider
                 options={{
-                  "client-id":
-                    "ATTL8fDJKfGzXNH4VVuDy1qW4_Jm8S0sqmnUTeYtWpqxUJLnXIn90V8YIGDg-SNPaB70Hg4mko_fde4-",
+                  "client-id": "test",
                   components: "buttons",
                   currency: "USD",
                   "disable-funding": "credit,card,p24",
